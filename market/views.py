06f67ccfac3be95430bbfa4fc48663d72bf111f5
g17938oy <g17938oy@gm.tsuda.ac.jp>
2019-10-09 13:57:56 +0900

    lesson modelをデータベースに反映する。

--- market/views.py
+++ market/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from .models import Book
 
 def book_list(request):
-    return render(request, 'market/book_list.html', {})
+    books = Book.objects.order_by('price')
+    return render(request, 'market/book_list.html', {'books':books})
