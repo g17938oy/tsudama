bffe6839ba997e56a0262e1346377477071ef65f
g17938oy <g17938oy@gm.tsuda.ac.jp>
2019-10-09 16:11:21 +0900

    追加する要素を決めたhtmlファイルを追加

--- market/views.py
+++ market/views.py
@@ -3,4 +3,4 @@ from .models import Book
 
 def book_list(request):
     books = Book.objects.order_by('price')
-    return render(request, 'market/book_list.html', {'books':books})
+    return render(request, 'market/home.html', {'books':books})
